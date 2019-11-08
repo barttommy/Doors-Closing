627f63e5ce446622e7ebea1331f4c061adb51d5f
Tommy Bart <tommybart8@gmail.com>
2019-11-07 18:55:40 -0600

    Added location services and permission request

--- app/src/main/java/com/example/chicagotraintracker/MainActivity.java
+++ app/src/main/java/com/example/chicagotraintracker/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.chicagotraintracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
+
+    private static final int LOCATION_REQUEST_CODE = 123;
+    private LocationManager locationManager;
+    private Criteria criteria;
 
     private RecyclerView recyclerView;
     private RouteAdapter routeAdapter;
@@ -43,19 +59,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 });
 
+
         requestedStations.add(new Station("40380", "Clark/Lake", "", ""));
 //        requestedStations.add(new Station("41220", "Fullerton", "", ""));
 //        requestedStations.add(new Station("40660", "Armitage", "", ""));
 
         // TODO update request, check for internet connection, location permission
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        criteria = new Criteria();
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+
         doRefresh();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[0] == PERMISSION_GRANTED) {
+                doRefresh();
+            }
+        }
     }
 
     private void doRefresh() {
         Log.d(TAG, "doRefresh: Refreshing data");
         swiper.setRefreshing(true);
-        new AsyncArrivalsLoader(this, requestedStations).execute();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            swiper.setRefreshing(false);
+        } else {
+            String provider = locationManager.getBestProvider(criteria, true);
+            Location currentLocation = locationManager.getLastKnownLocation(provider);
+            Log.d(TAG, String.format("Current location: %s %s", currentLocation.getLatitude(), currentLocation.getLongitude()));
+            new AsyncArrivalsLoader(this, requestedStations).execute();
+        }
     }
 
     void acceptResults(ArrayList<Route> results) {
